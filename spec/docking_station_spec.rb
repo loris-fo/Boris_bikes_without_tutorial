f42c2523e2855197cf862f0e382e710ede2578a8
lorisfo <loris-fo@outlook.com>
2015-02-04 19:39:39 +0000

    added available-bikes list

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,10 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 		
-		let(:bike) {Bike.new}
-		let(:station) {DockingStation.new}
+		let(:bike){Bike.new}
+		let(:station){DockingStation.new}
 	
 	it 'should accept a bike' do
 		expect{station.dock(bike)}.to change{station.bike_count}.by 1
@@ -22,6 +23,14 @@ describe DockingStation do
 	it 'should not accept a bike when the station is full' do
 		20.times{station.dock(bike)}
 		expect{station.dock(bike)}.to raise_error 'the station is full'
+	end
+
+	it 'should give a list of available bikes' do
+		working_bike, broken_bike = Bike.new, Bike.new
+		broken_bike.break!
+		station.dock(working_bike)
+		station.dock(broken_bike)
+		expect(station.available_bikes).to eq([working_bike])
 	end
 
 end
