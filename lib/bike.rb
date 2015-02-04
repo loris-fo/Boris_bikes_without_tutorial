f4815173207363641538d81a284eb135ec3b3c0c
lorisfo <loris-fo@outlook.com>
2015-02-04 16:40:56 +0000

    added a docking station feature

--- lib/bike.rb
+++ lib/bike.rb
@@ -12,6 +12,6 @@ class Bike
 		@broken = true
 	end
 
-
-
 end
+
+ 
---
+++ lib/docking_station.rb
@@ -0,0 +1,15 @@
+class DockingStation
+
+	def initialize
+		@bikes = []
+	end
+
+	def dock(bike)
+		@bikes << bike
+	end
+
+	def bike_count
+		@bikes.count
+	end
+
+end
