8bbbeb88734a340ef427c1072d8d324d6719c561
lorisfo <loris-fo@outlook.com>
2015-02-04 14:07:27 +0000

    added bike creation and breaking bike features

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,3 +1,15 @@
 class Bike
 
+	def initialize
+		@broken
+	end
+
+	def broken?
+		@broken
+	end
+
+	def break!
+		@broken = true
+	end
+
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,17 +4,14 @@ require 'bike'
 
 describe Bike do
 	
-	it 'should not be broken when it\'s created' do
-
+	it 'should be created without being broken' do
+		bike = Bike.new
+		expect(bike).not_to be_broken
 	end
 
-
-
-
-
-
-
-
-
-
+	it 'should break' do
+		bike = Bike.new
+		bike.break!
+		expect(bike).to be_broken
+	end
 end
